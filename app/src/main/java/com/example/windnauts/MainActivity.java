401a8f9e77380512894ae2b7c2963e93d4c4b7b9
Shunya OKINO <okinoshun1512@gmail.com>
2020-08-19 09:53:06 +0900

    バグの修正

--- app/src/main/java/com/example/windnauts/MainActivity.java
+++ app/src/main/java/com/example/windnauts/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
-        //ActiveDataListener();
+        ActiveDataListener();
     }
 
     @Override
