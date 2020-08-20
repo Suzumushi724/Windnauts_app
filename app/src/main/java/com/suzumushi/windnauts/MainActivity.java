c66643dbb14fb97197e60cae057ea28fc6439506
Shunya OKINO <okinoshun1512@gmail.com>
2020-08-20 13:26:05 +0900

    一日複数回の記録を可能にした

--- app/src/main/java/com/suzumushi/windnauts/MainActivity.java
+++ app/src/main/java/com/suzumushi/windnauts/MainActivity.java
@@ -26,6 +26,7 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
@@ -82,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
         else if(requestCode == SCAN_CODE){
             IntentResult result = IntentIntegrator.parseActivityResult(resultCode, data);
             if(result != null) {
-                Log.d("test","success");
                 if(result.getContents() == null) {
                     Toast.makeText(this,"読み取り失敗",Toast.LENGTH_LONG);
                 } else {
@@ -94,22 +94,21 @@ public class MainActivity extends AppCompatActivity {
                             public void onDataChange(@NonNull DataSnapshot snapshot) {
                                 Long status_num = (Long) snapshot.child("Active").getValue();
                                 if(status_num != null) {
-                                    if(!snapshot.child("Data").child(today).child("in").exists()) {
+
                                         if (status_num == 0) {
-                                            in();
+                                            inout(snapshot,IN_CODE,!snapshot.child("Data").child(today).child("in").exists());
                                             Toast.makeText(MainActivity.this, "入室を記録しました", Toast.LENGTH_LONG).show();
                                         } else {
                                             Toast.makeText(MainActivity.this, "前回の退出記録がありません", Toast.LENGTH_LONG).show();
                                         }
-                                    }
-                                    else{
-                                        Toast.makeText(MainActivity.this,"記録できるのは1日に1回までです",Toast.LENGTH_LONG).show();
-                                    }
                                 }
+                                /*
                                 else{
-                                    in();
+                                    in(snapshot);
                                     Toast.makeText(MainActivity.this,"入室を記録しました",Toast.LENGTH_LONG).show();
                                 }
+
+                                 */
                             }
 
                             @Override
@@ -124,17 +123,14 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot snapshot) {
                                 Long status_num = (Long)snapshot.child("Active").getValue();
-                                if(status_num != null) {
-                                    if (status_num == 1) {
-                                        out();
-                                        Toast.makeText(MainActivity.this, "退室を記録しました", Toast.LENGTH_LONG).show();
-                                    } else {
-                                        Toast.makeText(MainActivity.this, "前回の入室記録がありません", Toast.LENGTH_LONG).show();
-                                    }
+
+                                if (status_num == 1) {
+                                    inout(snapshot,OUT_CODE,!snapshot.child("Data").child(today).child("out").exists());
+                                    Toast.makeText(MainActivity.this, "退室を記録しました", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "前回の入室記録がありません", Toast.LENGTH_LONG).show();
                                 }
-                                else{
-                                    Toast.makeText(MainActivity.this,"入室から記録してください",Toast.LENGTH_LONG).show();
-                                }
+
                             }
 
                             @Override
@@ -178,21 +174,15 @@ public class MainActivity extends AppCompatActivity {
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                     Map<String, String> value = new HashMap<>();
                     value = (Map<String, String>) dataSnapshot.getValue();
-                    Log.d(TAG, "Value is: " + value);
                     if (value != null) {
                         if (!value.containsKey(user.getUid())) {
-                            DatabaseReference member = database.getReference("members").child(user.getUid()).child("name");
-                            member.setValue(user.getDisplayName());
-                            TextView user_name = findViewById(R.id.user_name);
-                            user_name.setText(user.getDisplayName() + getString(R.string.welcome));
-
+                            init_user(user,database);
                         } else {
                             TextView user_name = findViewById(R.id.user_name);
                             user_name.setText(user.getDisplayName() + getString(R.string.come_back));
                         }
                     } else {
-                        DatabaseReference member = database.getReference("members").child(user.getUid()).child("name");
-                        member.setValue(user.getDisplayName());
+                        init_user(user,database);
                     }
                 }
 
@@ -206,18 +196,33 @@ public class MainActivity extends AppCompatActivity {
         return user;
     }
 
-    public void in(){
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        database.getReference(
-                "members").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("Active")
-                .setValue(1);
-        Date in_time = new Date();
-        String Data = in_time.toString();
-        database.getReference(
-                "members").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("Data").child(today).child("in")
-                .setValue(Data);
+    public void inout(DataSnapshot snapshot,String code,boolean first_time){
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(
+                "members").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        if(code.equals(IN_CODE)) {
+            ref.child("Active").setValue(1);
+        }
+        else{
+            ref.child("Active").setValue(0);
+        }
+        Date time = new Date();
+        String Data = time.toString();
+        if(first_time){
+            ref.child("Data").child(today).child(code).setValue(new ArrayList<String>(){
+                {
+                    add(Data);
+                }
+            });
+            return;
+        }
+        else{
+            List<String> data_list = (List<String>) snapshot.child("Data").child(today).child(code).getValue();
+            data_list.add(Data);
+            ref.child("Data").child(today).child(code).setValue(data_list);
+            return;
+        }
     }
-
+/*
     public void out(){
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         database.getReference(
@@ -229,6 +234,8 @@ public class MainActivity extends AppCompatActivity {
                 "members").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("Data").child(today).child("out")
                 .setValue(Data);
     }
+
+ */
 
     public void ActiveDataListener(){
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference("members");
@@ -255,4 +262,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void init_user(FirebaseUser user, FirebaseDatabase database){
+        DatabaseReference member = database.getReference("members").child(user.getUid());
+        member.child("name").setValue(user.getDisplayName());
+        member.child("Active").setValue(0);
+        TextView user_name = findViewById(R.id.user_name);
+        user_name.setText(user.getDisplayName() + getString(R.string.welcome));
+    }
+
 }
